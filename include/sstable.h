a2071ce60d303c15e1eabd01c9f2f7f97a8f7d93
Justin Smith <justnwsmith@gmail.com>
2025-12-27 23:04:08 -1000

    Added sequence number + metadata to SSTable entries
    New SSTable file layout looks like:
        [key_size][value_size][key][value]...
        end of file has [min_key_len][max_key_len][min_key][max_key][offset]

--- include/sstable.h
+++ include/sstable.h
@@ -1,6 +1,7 @@
 #ifndef SSTABLE_H
 #define SSTABLE_H
 
+#include "types.h"
 #include <cstdint>
 #include <filesystem>
 #include <fstream>
@@ -18,7 +19,7 @@ class SSTable {
 
     // Constructor for creating a new SSTable from a MemTable snapshot
     // flush_counter is used to generate the filename
-    static SSTable flush(const std::map<std::string, std::string> &snapshot, const std::string &dir_path, uint64_t flush_counter);
+    static SSTable flush(const std::map<std::string, Entry> &snapshot, const std::string &dir_path, uint64_t flush_counter);
 
     // Retrieve a value by key. Returns std::nullopt if key not found.
     std::optional<std::string> get(const std::string &key) const;
@@ -26,12 +27,15 @@ class SSTable {
     // Return the filename for this SSTable
     std::string filename() const;
 
-    std::map<std::string, std::string> getData() const;
+    std::map<std::string, Entry> getData() const;
 
   private:
     std::string path_;
     std::string min_key_;
     std::string max_key_;
+    uint64_t metadata_offset_;
+
+    void loadMetadata();
 };
 
 #endif
--- src/sstable.cpp
+++ src/sstable.cpp
@@ -3,37 +3,57 @@
 // TODO: Hot keys cache implementation
 
 SSTable::SSTable(const std::string &path) : path_(path) {
+    loadMetadata();
 }
 
-SSTable SSTable::flush(const std::map<std::string, std::string> &snapshot, const std::string &dir_path, uint64_t flush_counter) {
+SSTable SSTable::flush(const std::map<std::string, Entry> &snapshot, const std::string &dir_path, uint64_t flush_counter) {
     std::string full_path = dir_path + "sstable_" + std::to_string(flush_counter) + ".bin";
     std::filesystem::create_directories(dir_path);
+    SSTable table(full_path);
 
     std::ofstream sstableFile(full_path, std::ios::out | std::ios::binary);
     if (!sstableFile) {
         throw std::runtime_error("Failed to open SSTable file: " + full_path);
     }
 
-    for (const auto &[key, value] : snapshot) {
-        uint16_t keyLen = key.size();
-        uint16_t valueLen = value.size();
-        const char *keyBytes = key.data();
-        const char *valueBytes = value.data();
+    for (const auto &[k, v] : snapshot) {
+        uint32_t keyLen = k.size();
+        uint32_t valueLen = v.value.size();
+        const char *keyBytes = k.data();
+        const char *valueBytes = v.value.data();
 
+        sstableFile.write(reinterpret_cast<const char *>(&v.seq), sizeof(v.seq));
         sstableFile.write(reinterpret_cast<const char *>(&keyLen), sizeof(keyLen));
         sstableFile.write(reinterpret_cast<const char *>(&valueLen), sizeof(valueLen));
         sstableFile.write(keyBytes, keyLen);
         sstableFile.write(valueBytes, valueLen);
     }
-    SSTable result(full_path);
-    result.min_key_ = snapshot.begin()->first;
-    result.max_key_ = snapshot.rbegin()->first;
-    return result;
+
+    table.min_key_ = snapshot.begin()->first;
+    table.max_key_ = snapshot.rbegin()->first;
+
+    uint32_t minKeyLen = table.min_key_.size();
+    uint32_t maxKeyLen = table.max_key_.size();
+
+    table.metadata_offset_ = sstableFile.tellp();
+
+    sstableFile.write(reinterpret_cast<const char *>(&minKeyLen), sizeof(minKeyLen));
+    sstableFile.write(reinterpret_cast<const char *>(&maxKeyLen), sizeof(maxKeyLen));
+
+    sstableFile.write(table.min_key_.data(), minKeyLen);
+    sstableFile.write(table.max_key_.data(), maxKeyLen);
+
+    sstableFile.write(reinterpret_cast<const char *>(&table.metadata_offset_), sizeof(table.metadata_offset_));
+
+    return table;
 }
 
 std::optional<std::string> SSTable::get(const std::string &key) const {
-    if (key < min_key_ || key > max_key_)
+    std::cout << min_key_ << " " << max_key_ << std::endl;
+    if (key < min_key_ || key > max_key_) {
+        std::cout << "here5";
         return std::nullopt;
+    }
 
     std::ifstream sstableFile(path_, std::ios::in | std::ios::binary);
 
@@ -41,17 +61,21 @@ std::optional<std::string> SSTable::get(const std::string &key) const {
         throw std::runtime_error("Failed to open SSTable file: " + path_);
     }
 
-    uint16_t keyLenBytes{};
-    uint16_t valueLenBytes{};
+    uint64_t seqNumBytes{};
+    uint32_t keyLenBytes{};
+    uint32_t valueLenBytes{};
 
     while (true) {
-        if (!sstableFile.read(reinterpret_cast<char *>(&keyLenBytes), sizeof(keyLenBytes))) {
+
+        if (!sstableFile.read(reinterpret_cast<char *>(&seqNumBytes), sizeof(seqNumBytes))) {
             break;
         }
+        sstableFile.read(reinterpret_cast<char *>(&keyLenBytes), sizeof(keyLenBytes));
         sstableFile.read(reinterpret_cast<char *>(&valueLenBytes), sizeof(valueLenBytes));
 
-        uint16_t keyLen = static_cast<uint16_t>(keyLenBytes);
-        uint16_t valueLen = static_cast<uint16_t>(valueLenBytes);
+        // uint64_t seqNum = static_cast<uint32_t>(seqNumBytes);
+        uint32_t keyLen = static_cast<uint32_t>(keyLenBytes);
+        uint32_t valueLen = static_cast<uint32_t>(valueLenBytes);
 
         std::string currKey;
         std::string currValue;
@@ -61,6 +85,7 @@ std::optional<std::string> SSTable::get(const std::string &key) const {
         sstableFile.read(reinterpret_cast<char *>(&currKey[0]), keyLen);
         sstableFile.read(reinterpret_cast<char *>(&currValue[0]), valueLen);
 
+        std::cout << "KEYS: " << currKey << " " << key << std::endl;
         if (currKey == key) {
             return currValue;
         }
@@ -68,8 +93,8 @@ std::optional<std::string> SSTable::get(const std::string &key) const {
     return std::nullopt;
 }
 
-std::map<std::string, std::string> SSTable::getData() const {
-    std::map<std::string, std::string> sstableData;
+std::map<std::string, Entry> SSTable::getData() const {
+    std::map<std::string, Entry> sstableData;
 
     std::ifstream sstableFile(path_, std::ios::in | std::ios::binary);
 
@@ -77,27 +102,57 @@ std::map<std::string, std::string> SSTable::getData() const {
         throw std::runtime_error("Failed to open SSTable file: " + path_);
     }
 
-    uint16_t keyLenBytes{};
-    uint16_t valueLenBytes{};
+    uint64_t seqNumBytes{};
+    uint32_t keyLenBytes{};
+    uint32_t valueLenBytes{};
 
     while (true) {
-        if (!sstableFile.read(reinterpret_cast<char *>(&keyLenBytes), sizeof(keyLenBytes))) {
+        if (!sstableFile.read(reinterpret_cast<char *>(&seqNumBytes), sizeof(seqNumBytes)) ||
+            !sstableFile.read(reinterpret_cast<char *>(&keyLenBytes), sizeof(keyLenBytes)) ||
+            !sstableFile.read(reinterpret_cast<char *>(&valueLenBytes), sizeof(valueLenBytes))) {
             break;
         }
-        sstableFile.read(reinterpret_cast<char *>(&valueLenBytes), sizeof(valueLenBytes));
 
-        uint16_t keyLen = static_cast<uint16_t>(keyLenBytes);
-        uint16_t valueLen = static_cast<uint16_t>(valueLenBytes);
+        uint64_t seqNum = static_cast<uint32_t>(seqNumBytes);
+        uint32_t keyLen = static_cast<uint32_t>(keyLenBytes);
+        uint32_t valueLen = static_cast<uint32_t>(valueLenBytes);
 
         std::string currKey;
         std::string currValue;
         currKey.resize(keyLen);
         currValue.resize(valueLen);
 
-        sstableFile.read(reinterpret_cast<char *>(&currKey[0]), keyLen);
-        sstableFile.read(reinterpret_cast<char *>(&currValue[0]), valueLen);
+        if (!sstableFile.read(reinterpret_cast<char *>(&currKey[0]), keyLen) ||
+            !sstableFile.read(reinterpret_cast<char *>(&currValue[0]), valueLen)) {
+            break;
+        }
 
-        sstableData[currKey] = currValue;
+        sstableData[currKey].value = currValue;
+        sstableData[currKey].seq = seqNum;
     }
     return sstableData;
+}
+
+void SSTable::loadMetadata() {
+    std::ifstream sstableFile(path_, std::ios::binary);
+    if (!sstableFile)
+        return;
+
+    sstableFile.seekg(-static_cast<int>(sizeof(uint64_t)), std::ios::end);
+
+    sstableFile.read(reinterpret_cast<char *>(&metadata_offset_), sizeof(metadata_offset_));
+
+    sstableFile.seekg(metadata_offset_);
+
+    uint32_t minKeyLen, maxKeyLen;
+    sstableFile.read(reinterpret_cast<char *>(&minKeyLen), sizeof(minKeyLen));
+    min_key_.resize(minKeyLen);
+    sstableFile.read(reinterpret_cast<char *>(&maxKeyLen), sizeof(maxKeyLen));
+    max_key_.resize(maxKeyLen);
+
+    sstableFile.read(&min_key_[0], minKeyLen);
+    sstableFile.read(&max_key_[0], maxKeyLen);
+
+    std::cout << "MIN KEY: " << min_key_ << std::endl;
+    std::cout << "MAX KEY: " << max_key_ << std::endl;
 }
